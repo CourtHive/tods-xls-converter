f225c3bb6e9337c8d02b1a7f5c984bb2a05fd686
CourtHive <charles@courthive.com>
2022-11-22 07:59:45 -0500

    fix factory import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { identifyWorkbook } from './functions/identifyWorkbook';
 import { processDirectory } from './utilities/processDirectory';
 import { processSheets } from './functions/processSheets';
-import { factory } from 'tods-competition-factory';
+import * as factory from 'tods-competition-factory';
 import { loadWorkbook } from './global/loader';
 import globalLog from './utilities/globalLog';
 import xlsState from './global/state';
