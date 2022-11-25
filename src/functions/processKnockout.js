cc582fe7e66d575cb3cd2c581ca776546c65227f
CourtHive <charles@courthive.com>
2022-11-25 13:29:51 -0500

    `processKnockout`: add `processPreRound` and beging round processing

--- src/functions/processKnockout.js
+++ src/functions/processKnockout.js
@@ -1,7 +1,7 @@
 import { getPositionColumn } from '../utilities/convenience';
-import { utilities } from 'tods-competition-factory';
 import { getRoundMatchUps } from './getRoundMatchUp';
 import { getPositionRows } from './getPositionRows';
+import { processPreRound } from './processPreRound';
 
 import { PRE_ROUND } from '../constants/columnConstants';
 import { SUCCESS } from '../constants/resultConstants';
@@ -20,30 +20,65 @@ export function processKnockOut({ sheetDefinition, profile, analysis, sheet, inf
     preRoundColumn
   });
 
-  const qualifyingStructure = {},
-    structure = {},
-    matchUps = [];
+  const participants = [],
+    structures = [],
+    matchUps = [],
+    links = [];
 
   // *. If preRound, use `preRoundParticipantRows` and positionRows[0] to see whether there are progressed participants and set first roundNumber column
   //    - preRound is roundNumber: 0, first round of structure is roundNumber: 1
 
   if (preRoundParticipantRows?.length) {
-    const pairedPositions = utilities.chunkArray(preRoundParticipantRows, 2);
-    // these matchUps will go into qualifyingStructure
-    qualifyingStructure.matchUps = getRoundMatchUps({
-      column: preRoundColumn,
-      pairedPositions,
-      roundNumber: 1,
-      analysis
+    const { matchUps, structure, participantDetails } = processPreRound({
+      preRoundParticipantRows,
+      preRoundColumn,
+      analysis,
+      profile
     });
 
-    matchUps.push(...qualifyingStructure.matchUps);
+    structures.push(structure);
+
+    participants.push(...participantDetails.filter(({ isByePosition }) => isByePosition));
+
+    matchUps.push(...matchUps);
   }
+
+  const qualifyingParticipants = participants.filter(({ advancedParticipantName }) => advancedParticipantName);
 
   // *. if no preRound, check whether there are values present in the valuesMap on positionRows[0] of first column after the position round
   //    - check whether there are progressed particpants in positionRows[1]
   //    - in rare cases there may be a preRound column BEFORE the position column... if position column > A this could be true
   //    - if there is a column before positionRound see whether any of the positioned values of roundNumber: 1 are present in that coulmn
+
+  const roundColumnsToProcess = analysis.columnProfiles
+    .filter(({ column }) => ![preRoundColumn, positionColumn].filter(Boolean).includes(column))
+    .map(({ column }) => column);
+
+  roundColumnsToProcess.forEach((column, i) => {
+    const pairedRowNumbers = positionProgression[i];
+
+    if (pairedRowNumbers) {
+      const { matchUps: roundMatchUps, participantDetails } = getRoundMatchUps({
+        pairedRowNumbers,
+        roundNumber: 1,
+        analysis,
+        profile,
+        column
+      });
+
+      if (roundMatchUps) {
+        console.log({ roundMatchUps, participantDetails });
+      }
+    } else {
+      /*
+      // diagnostics
+      const profile = analysis.columnProfiles.find((c) => c.column === column);
+      const keys = Object.keys(profile.keyMap);
+      console.log({ column, i, profile });
+      keys.forEach((key) => console.log(sheet[key]));
+      */
+    }
+  });
 
   Object.assign(analysis, {
     preRoundParticipantRows,
@@ -51,12 +86,12 @@ export function processKnockOut({ sheetDefinition, profile, analysis, sheet, inf
     positionRows
   });
 
-  return { analysis, info, hasValues: true, ...SUCCESS };
+  return { analysis, info, links, structures, matchUps, hasValues: true, ...SUCCESS };
 
   // NOTES:
   // *. Is there a pre-round
   // *. Use preRoundParticipantRows to create Qualifying Structure with matchUps
-  // *.results can be inferred by looking at columngProfile keyMap values which occur between positionRows
+  // *. results can be inferred by looking at columngProfile keyMap values which occur between positionRows
   // *. Characterize { drawSize: ##, R: 32, 16, 8. 4. 3 }
   // *. For each round, does the previous round have matching names
   // *. Using matching values across rounds calculate where progressing values should occur (to correct for those which have misspellings)
@@ -68,106 +103,4 @@ export function processKnockOut({ sheetDefinition, profile, analysis, sheet, inf
   // *. Do all rounds have comma separated alpha values?
   // *. If there is a pre-round, are there results/scores in the first round participant column
   // *. Go back to each round and get date/times which occur on rows between paired paritipants
-
-  /*
-    const matchUpAttributes = { roundNumber: 1, roundPosition: 1, drawSize: 32, participantNames: ['Ray', 'Charles'] };
-    const matchUpId = generateMatchUpId(matchUpAttributes);
-    console.log({ matchUpId });
-  */
-
-  /*
-  analysis.rowGroupings.forEach((grouping) => {
-    const { columns, attributes, rowCount } = grouping;
-    pushGlobalLog({ columns, attributes, rowCount });
-  });
-  */
-
-  /*
-  const result = getParticipantRows({
-    headerRow,
-    footerRow,
-    avoidRows,
-    profile,
-    columns,
-    sheet
-  });
-  // console.log({ result });
-  const { rows, range, finals, preRoundRows } = result;
-  const { players, isDoubles } = extractKnockOutParticipants({
-    preRoundRows,
-    headerRow,
-    columns,
-    profile,
-    gender,
-    sheet,
-    range,
-    finals,
-    rows
-  });
-  console.log({
-    headerRow,
-    columns,
-    rows,
-    range,
-    finals,
-    preRoundRows,
-    profile
-  });
-  const drawFormat = isDoubles ? 'DOUBLES' : 'SINGLES';
-
-  const playerData = { players, rows, range, finals, preRoundRows };
-  const { matchUps, stage } = constructKnockOut({
-    profile,
-    sheet,
-    columns,
-    headerRow,
-    gender,
-    playerData
-  });
-  const { entries, playersMap, participantsMap, positionAssignments, seedAssignments } = getEntries({
-    matchUps,
-    drawFormat
-  });
-
-  Object.assign(drawInfo, { drawFormat, stage });
-  const sizes = [matchUps, entries, positionAssignments, seedAssignments].map((v) => v.length);
-  const fodder = sizes
-    .concat(...Object.values(drawInfo).filter((v) => typeof v === 'string'))
-    .sort()
-    .join('');
-  const drawId = hashId(fodder);
-
-  const TodsMatchUps = matchUps.map((matchUp) => {
-    const drawPositions = matchUp.drawPositions.sort((a, b) => a - b);
-    const matchUpId = `${drawId}-${drawPositions.join('')}-M`;
-    const winningSide = drawPositions.indexOf(matchUp.winningDrawPosition) + 1;
-    return {
-      matchUpId,
-      drawPositions,
-      score: matchUp.result,
-      roundName: matchUp.roundName,
-      roundNumber: matchUp.roundNumber,
-      roundPosition: matchUp.roundPosition,
-      finishingRound: matchUp.finishingRound,
-      winningSide
-    };
-  });
-
-  const structureIdFodder = `${fodder}${stage}`;
-  const structureId = `${hashId(structureIdFodder)}-S`;
-  const structure = {
-    stage,
-    structureId,
-    stageSequence: 1,
-    seedAssignments,
-    positionAssignments,
-    matchUps: TodsMatchUps,
-    finishingPosition: 'roundOutcome'
-  };
-
-  Object.assign(drawInfo, { drawId, stage, matchUps, structure, entries });
-  matchUps.forEach((matchUp) => (matchUp.event = drawInfo.event));
-
-  return { drawInfo, playersMap, participantsMap, ...SUCCESS };
-  */
 }
---
+++ src/functions/processPreRound.js
@@ -0,0 +1,63 @@
+import { generateMatchUpId, generateParticipantId } from '../utilities/hashing';
+import { drawDefinitionConstants, utilities } from 'tods-competition-factory';
+import { getRoundMatchUps } from './getRoundMatchUp';
+
+const { QUALIFYING } = drawDefinitionConstants;
+
+export function processPreRound({ preRoundParticipantRows, preRoundColumn, analysis, profile }) {
+  const pairedRowNumbers = utilities.chunkArray(preRoundParticipantRows, 2);
+  // these matchUps will go into qualifyingStructure
+  const { matchUps, participantDetails } = getRoundMatchUps({
+    column: preRoundColumn,
+    isPreRound: true,
+    pairedRowNumbers,
+    roundNumber: 1,
+    analysis,
+    profile
+  });
+
+  for (const matchUp of matchUps) {
+    const { roundPosition } = matchUp;
+    const firstDrawPosition = (roundPosition - 1) * 2;
+    matchUp.drawPositions = [firstDrawPosition + 1, firstDrawPosition + 2];
+  }
+
+  const drawSize = Math.max(...matchUps.flatMap(({ drawPositions }) => drawPositions));
+
+  const positionAssignments = [];
+
+  // preRound should have no BYEs
+  for (const participant of participantDetails) {
+    const { isByePosition, participantName, roundPosition, sideNumber } = participant;
+    const matchUp = matchUps.find((matchUp) => roundPosition === matchUp.roundPosition);
+    const drawPosition = matchUp.drawPositions[sideNumber - 1];
+
+    if (isByePosition) {
+      positionAssignments.push({ bye: true, drawPosition });
+    } else {
+      const { participantId } = generateParticipantId({ attributes: [participantName] });
+      const positionAssignment = { participantId, drawPosition };
+      positionAssignments.push(positionAssignment);
+
+      participant.participantId = participantId;
+    }
+  }
+
+  for (const matchUp of matchUps) {
+    const { matchUpId } = generateMatchUpId({
+      additionalAttributes: [analysis.sheetName, ...analysis.multiColumnValues],
+      // this will be the unique component for this sheet/structure in the generator
+      participantNames: matchUp.pairParticipantNames,
+      drawSize
+    });
+    matchUp.matchUpId = matchUpId;
+  }
+
+  const structureName = 'Pre-Qualifying';
+  const stage = QUALIFYING;
+  const stageSequence = 1;
+
+  const structure = { matchUps, structureName, stage, stageSequence, positionAssignments };
+
+  return { matchUps, participantDetails, structure };
+}
