2e3d573a2ee4a17029c7ac4444a5600bafc592af
CourtHive <charles@courthive.com>
2022-11-25 13:28:48 -0500

    `getRoundMatchUps`: WIP

--- src/functions/getRoundMatchUp.js
+++ src/functions/getRoundMatchUp.js
@@ -1,47 +1,138 @@
-export function getRoundMatchUps({ analysis, column, pairedPositions, roundNumber }) {
+import { getNonBracketedValue, withoutQualifyingDesignator } from '../utilities/convenience';
+import { matchUpStatusConstants } from 'tods-competition-factory';
+
+const { BYE, COMPLETED } = matchUpStatusConstants;
+
+export function getRoundMatchUps({ pairedRowNumbers, roundNumber, isPreRound, analysis, profile, column }) {
   const isWholeNumber = (num) => num % 1 === 0;
   const getColumn = (column) =>
-    analysis.columnProfiles.reduce((profile, currentProfile, index) => {
-      return profile || (currentProfile.column === column && { profile: currentProfile, index });
+    analysis.columnProfiles.reduce((columnProfile, currentProfile, index) => {
+      return columnProfile || (currentProfile.column === column && { columnProfile: currentProfile, index });
     }, undefined);
 
-  const { profile, index } = getColumn(column);
+  const { matchUpStatuses } = profile;
+  const { columnProfile, index } = getColumn(column);
   const nextColumnProfile = analysis.columnProfiles[index + 1];
+  if (!nextColumnProfile) return {};
 
+  const nextColumnResults = analysis.columnResultValues[nextColumnProfile.column] || [];
+
+  const participantDetails = [];
   const matchUps = [];
-  for (const pair of pairedPositions) {
-    const nextPosition = Math.abs(pair[1] - pair[0]) / 2 + Math.min(...pair);
-    if (isWholeNumber(nextPosition)) {
-      // get participants from column
-      const pairParticipantNames = pair.map((position) => {
-        const ref = `${profile.column}${position}`;
-        return profile.keyMap[ref];
-      });
+
+  let roundPosition = 1;
+
+  for (const pair of pairedRowNumbers) {
+    // get participants from column
+    const matchUpParticipants = [];
+    const pairParticipantNames = pair.map((rowNumber, i) => {
+      const ref = `${columnProfile.column}${rowNumber}`;
+      const participantName = columnProfile.keyMap[ref];
+
+      // drawPosition for first round can be derived from (roundPosition - 1) * 2 + sideNumber
+      const isByePosition = participantName === matchUpStatuses?.bye;
+      if (isByePosition) {
+        matchUpParticipants.push({ isByePosition, rowNumber, roundPosition, sideNumber: i + 1 });
+      } else {
+        matchUpParticipants.push({ participantName, rowNumber, roundPosition, sideNumber: i + 1 });
+      }
+
+      return participantName;
+    });
+
+    const nextColumnRowNumber = Math.abs(pair[1] - pair[0]) / 2 + Math.min(...pair);
+    if (isWholeNumber(nextColumnRowNumber)) {
       const nextColumn = nextColumnProfile.column;
-      const winningParticipantName = nextColumnProfile.keyMap[`${nextColumn}${nextPosition}`];
-      const { winningSide } = getWinningSide({ pairParticipantNames, winningParticipantName });
-      console.log({ pairParticipantNames, winningParticipantName, roundNumber, winningSide });
+      const nextColumnRef = `${nextColumn}${nextColumnRowNumber}`;
+      const winningParticipantName = nextColumnProfile.keyMap[nextColumnRef];
+      const { advancedSide } = getAdvancedSide({
+        winningParticipantName,
+        pairParticipantNames,
+        analysis,
+        profile
+      });
+      if (advancedSide) {
+        matchUpParticipants[advancedSide - 1].advancedParticipantName = winningParticipantName;
+        matchUpParticipants[advancedSide - 1].advancedPositionRef = nextColumnRef;
+      }
+      // get potential result
+      const potentialResult = nextColumnProfile.keyMap[`${nextColumn}${nextColumnRowNumber + 1}`];
+      const result = nextColumnResults.includes(potentialResult) && potentialResult;
+
+      const matchUp = { roundNumber, roundPosition, pairParticipantNames };
+      if (result) matchUp.result = result;
+
+      const providerBye = columnProfile.matchUpStatuses?.bye || BYE;
+      const isBye = pairParticipantNames.map((name) => name.toLowerCase()).includes(providerBye);
+
+      if (isBye) {
+        matchUp.matchUpStatus = BYE;
+      } else {
+        matchUp.matchUpStatus = COMPLETED;
+        matchUp.winningSide = advancedSide;
+      }
+
+      if (!result && !isBye) {
+        // TODO: in some draws preRound results appear as part of advancedSide participantName
+        if (isPreRound) {
+          console.log('check for result at end of advancedSide participantName');
+        } else if (matchUp.winningSide) {
+          console.log('No win reason', { matchUp });
+        }
+      }
+
+      matchUps.push(matchUp);
     }
+
+    participantDetails.push(...matchUpParticipants);
+
+    roundPosition += 1;
   }
 
-  return { matchUps };
+  return { matchUps, participantDetails };
 }
 
-function getWinningSide({ pairParticipantNames, winningParticipantName }) {
+function getAdvancedSide({ pairParticipantNames, winningParticipantName, analysis, profile }) {
   if (!winningParticipantName) return {};
-  const exactMatchSideNumber = pairParticipantNames.reduce((sideNumber, participantName, i) => {
-    const exactMatch = participantName === winningParticipantName;
-    return sideNumber || (exactMatch ? i + 1 : undefined);
-  }, undefined);
-  const startsWithSideNumber = pairParticipantNames.reduce((sideNumber, participantName, i) => {
-    const startsWith = participantName.startsWith(winningParticipantName);
-    return sideNumber || (startsWith ? i + 1 : undefined);
-  }, undefined);
-  const includesWithSideNumber = pairParticipantNames.reduce((sideNumber, participantName, i) => {
-    const includes = participantName.includes(winningParticipantName);
-    return sideNumber || (includes ? i + 1 : undefined);
-  }, undefined);
+  const { qualifyingIdentifiers } = profile;
+  if (analysis.isDoubles) {
+    //
+  }
 
-  const winningSide = exactMatchSideNumber || startsWithSideNumber || includesWithSideNumber;
-  return { winningSide };
+  const nonBracketedParticipantNames = pairParticipantNames.map((name) => {
+    const withoutSeeding = getNonBracketedValue(name);
+    return withoutQualifyingDesignator(withoutSeeding, qualifyingIdentifiers);
+  });
+  const nonBracketedWinningParticipantName = getNonBracketedValue(winningParticipantName);
+
+  const exactMatchSide = nonBracketedParticipantNames.reduce((side, participantName, i) => {
+    const condition = participantName === nonBracketedWinningParticipantName;
+    if (condition) {
+      return { advancedSide: i + 1, participantName };
+    } else {
+      return side;
+    }
+  }, {});
+  if (exactMatchSide?.advancedSide) return exactMatchSide;
+
+  const startsWith = nonBracketedParticipantNames.reduce((side, participantName, i) => {
+    const condition = participantName.startsWith(nonBracketedWinningParticipantName);
+    if (condition) {
+      return { advancedSide: i + 1, participantName };
+    } else {
+      return side;
+    }
+  }, {});
+  if (startsWith?.advancedSide) return startsWith;
+
+  const includes = nonBracketedParticipantNames.reduce((side, participantName, i) => {
+    const condition = participantName.includes(nonBracketedWinningParticipantName);
+    if (condition) {
+      return { advancedSide: i + 1, participantName };
+    } else {
+      return side;
+    }
+  }, {});
+
+  return includes || {};
 }
