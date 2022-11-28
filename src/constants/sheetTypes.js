5e81a1e2388dc70227efdf6364f3d9b833b2369c
CourtHive <charles@courthive.com>
2022-11-28 07:45:29 -0500

    add ORDER_OF_PLAY sheetType

--- src/constants/sheetTypes.js
+++ src/constants/sheetTypes.js
@@ -2,6 +2,7 @@ import { drawDefinitionConstants } from 'tods-competition-factory';
 
 export const ROUND_ROBIN = drawDefinitionConstants.ROUND_ROBIN;
 export const INDETERMINATE = 'INDETERMINATE';
+export const ORDER_OF_PLAY = 'ORDER_OF_PLAY';
 export const PARTICIPANTS = 'PARTICIPANTS';
 export const INFORMATION = 'INFORMATION';
 export const KNOCKOUT = 'KNOCKOUT';
