07b824919da3b9160a4035dccbeeebc70aa05953
CourtHive <charles@courthive.com>
2022-11-29 08:48:44 -0500

    `getCategory`: add `includesCategoryMatches`

--- src/functions/getCategory.js
+++ src/functions/getCategory.js
@@ -8,8 +8,18 @@ export function getCategory({ sheet, sheetName, profile }) {
   if (!categories?.length) return {};
 
   const exactCategoryMatches = findValueRefs({ searchDetails: categories, sheet, mapValues: true });
-  const sheetCategories = utilities.unique(Object.values(exactCategoryMatches).map(({ value }) => value));
+  const includesCategoryMatches = !Object.keys(exactCategoryMatches).length
+    ? findValueRefs({
+        options: { includes: true, requiredSeparator: ' ' },
+        searchDetails: categories,
+        mapValues: true,
+        sheet
+      })
+    : undefined;
 
+  const values = Object.values(includesCategoryMatches || exactCategoryMatches);
+
+  const sheetCategories = utilities.unique(values.map(({ value }) => value));
   const sheetNameCategory = sheetName && categories.find((category) => sheetName.includes(category));
 
   if (sheetCategories.length > 1) return { error: MULTIPLE_MATCHES };
@@ -18,7 +28,8 @@ export function getCategory({ sheet, sheetName, profile }) {
     return { error: VALUE_MISMATCH };
   }
 
-  const category = sheetCategories[0] || sheetNameCategory;
+  const loweredCategory = (sheetCategories[0] || sheetNameCategory)?.toLowerCase();
+  const category = categories.find((c) => loweredCategory?.includes(c.toLowerCase()));
 
   return { category };
 }
