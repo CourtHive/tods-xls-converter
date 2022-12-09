647bf1392d728cd3604015f9e770dd89112d7668
CourtHive <charles@courthive.com>
2022-12-09 08:13:01 -0500

    additional result identification function

--- src/utilities/identification.js
+++ src/utilities/identification.js
@@ -1,5 +1,33 @@
 export const hasNumeric = (value) => /\d+/.test(value);
 export const isString = (value) => typeof value === 'string';
 export const isNumeric = (value) => /^\d+(a)?$/.test(value);
-export const isScoreLike = (value) => /^[\d,-/() ]+[A-Za-z.]*$/.test(value);
+export const isScoreLike = (value) => {
+  return /^[\d,-/() ]+[A-Za-z.]*$/.test(excludeSingleDigits(value));
+};
 export const isObject = (value) => typeof value === 'object';
+export function excludeSingleDigits(value) {
+  return value
+    .toString()
+    .split(' ')
+    .filter((p) => !/^\d$/.test(p))
+    .join(' ');
+}
+export function splitValueOnFirstDigit(value) {
+  const parts = value.toString().split(' ');
+  const firstDigitPart = parts.find((part) => /\d/.test(part));
+  const firstDigitIndex = parts.indexOf(firstDigitPart);
+  return [parts.slice(0, firstDigitIndex).join(' '), parts.slice(firstDigitIndex).join(' ')];
+}
+export function digitsCount(value) {
+  return value
+    .toString()
+    .split('')
+    .map((v) => /\d/.test(v))
+    .filter(Boolean).length;
+}
+export function getPotentialResult(value) {
+  const splitValue = splitValueOnFirstDigit(value);
+  const potentialResult = excludeSingleDigits(splitValue[1]);
+  const isPotential = isScoreLike(potentialResult) && digitsCount(potentialResult) > 1;
+  return { leader: splitValue[0], potentialResult: isPotential && potentialResult };
+}
