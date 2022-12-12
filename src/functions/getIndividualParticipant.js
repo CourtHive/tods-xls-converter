b2315525b01277cb1a50e83ec728bb048c0cf23b
CourtHive <charles@courthive.com>
2022-12-12 11:50:43 -0500

    add qualifier identification

--- src/functions/getIndividualParticipant.js
+++ src/functions/getIndividualParticipant.js
@@ -9,12 +9,19 @@ const { COMPETITOR } = participantRoles;
 export function getIndividualParticipant({ name }) {
   let lastName, firstName;
 
+  // Costa Rica Qualifiers
+  const qTest = (name) => /^Q\d+\s/.test(name);
+  const qPositionTest = (name) => /^Q\d+$/.test(name);
+  const isQualifier = qTest(name);
+  const isQualifyingPosition = qPositionTest(name);
+  if (isQualifier || isQualifyingPosition) name = name.split(' ').slice(1).join(' ');
+
   if (name.includes(',')) {
     const parts = name.split(',').map((name) => normalizeName(name));
     lastName = parts[0];
     firstName = parts[1];
   } else {
-    const hasLowerStart = (n) => isString(n) && n[0] === n[0].toLowerCase() && n[0] !== n[0].toUpperCase();
+    const hasLowerStart = (n) => isString(n) && n[0] === n[0]?.toLowerCase() && n[0] !== n[0]?.toUpperCase();
     const parts = normalizeName(name).split(' ');
     let division = parts.map((part, index) => hasLowerStart(part) && index !== undefined && index).filter(Boolean)[0];
     if (!division) division = parts.length === 4 ? 2 : 1;
@@ -28,13 +35,15 @@ export function getIndividualParticipant({ name }) {
   const idAttributes = [firstName, lastName, participantName].filter(Boolean);
   const participantId = idAttributes.length && generateParticipantId({ attributes: idAttributes })?.participantId;
 
-  return {
+  const participant = {
     participantRole: COMPETITOR,
     participantType: INDIVIDUAL,
     participantName,
     participantId,
     person
   };
+
+  return { participant, isQualifyingPosition, isQualifier };
 }
 
 export function getPairParticipant({ individualParticipants }) {
