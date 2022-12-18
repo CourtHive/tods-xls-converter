5919bab1779b94132c242622dd8a711b2da6cfb0
CourtHive <charles@courthive.com>
2022-12-18 10:19:33 -0500

    `isScoreLike`: accept []

--- src/utilities/identification.js
+++ src/utilities/identification.js
@@ -2,7 +2,7 @@ export const hasNumeric = (value) => /\d+/.test(value);
 export const isString = (value) => typeof value === 'string';
 export const isNumeric = (value) => /^\d+(a)?$/.test(value);
 export const isScoreLike = (value) => {
-  return /^[\d,-/() ]+[A-Za-z.]*$/.test(excludeSingleDigits(value));
+  return /^[\d,-/() [\]]+[A-Za-z.]*$/.test(excludeSingleDigits(value));
 };
 export const isObject = (value) => typeof value === 'object';
 export function excludeSingleDigits(value) {
