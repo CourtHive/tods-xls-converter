8177d064bfc2f6c98b71739537a0c833d33db8af
CourtHive <charles@courthive.com>
2022-12-19 06:49:08 -0500

    extend pValues

--- src/functions/getParticipantValues.js
+++ src/functions/getParticipantValues.js
@@ -1,7 +1,9 @@
 const joiners = ['-', '/', ' / '];
 
 // extract all the various participant values that will be compared to advanced participant string
-export function getParticipantValues(participant) {
+export function getParticipantValues(participant, roundNumber, roundPosition) {
+  roundNumber && roundPosition; // usefult for debugging
+
   const { participantName, person, individualParticipants } = participant;
 
   let pValues = [participantName];
@@ -10,10 +12,17 @@ export function getParticipantValues(participant) {
     pValues.push(standardFamilyName, standardGivenName);
     // handle multiple last names where only one of the last names is progressed
     const splitFamilyName = standardFamilyName.split(' ');
+
+    // handle multiple last names where only one of the last names is progressed
     if (splitFamilyName !== standardFamilyName) pValues.push(...splitFamilyName);
+
+    // consider 'firstName lastName'
+    if (standardFamilyName && standardGivenName) pValues.push([standardGivenName, standardFamilyName].join(' '));
   }
-  const doublesFirstNames = [];
+
   const doublesLastNames = [];
+  const doublesFirstNames = [];
+
   individualParticipants?.forEach(({ person }) => {
     if (person) {
       const { standardFamilyName, standardGivenName } = person;
@@ -21,6 +30,9 @@ export function getParticipantValues(participant) {
       doublesLastNames.push(standardFamilyName);
 
       pValues.push(standardFamilyName);
+      // sometimes TDs progress given names rather than family names, or put family names in given name column
+      pValues.push(standardGivenName);
+
       // handle multiple last names where only one of the last names is progressed
       const splitFamilyName = standardFamilyName.split(' ');
       if (splitFamilyName !== standardFamilyName) pValues.push(...splitFamilyName);
@@ -33,5 +45,11 @@ export function getParticipantValues(participant) {
     pValues.push(...combinations);
   }
 
-  return pValues.filter(Boolean).map((v) => v.toString().toLowerCase());
+  return (
+    pValues
+      .filter(Boolean)
+      .map((v) => v.toString().toLowerCase())
+      // ignore values which are single characters
+      .filter((value) => value.length > 1)
+  );
 }
