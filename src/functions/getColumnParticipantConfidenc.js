5c12d406d3de6c98ae40fb9740354dc1e9a72f17
CourtHive <charles@courthive.com>
2022-12-19 08:52:26 -0500

    functional decomposition for reuse

---
+++ src/functions/getColumnParticipantConfidenc.js
@@ -0,0 +1,11 @@
+import { getParticipantValues } from './getParticipantValues';
+import { pRankReducer } from './pRankReducer';
+
+export function getColumnParticpantConfidence({ analysis, roundParticipants, targetColumn, confidenceThreshold }) {
+  const targetColumnProfile = analysis.columnProfiles.find(({ column }) => column === targetColumn);
+  const nextColumnValues = targetColumnProfile?.values;
+  const roundParticipantValues = roundParticipants.flat().map(getParticipantValues);
+  return nextColumnValues
+    ?.flatMap((value) => roundParticipantValues.map((pValues) => pRankReducer({ pValues, value, confidenceThreshold })))
+    .filter(({ confidence }) => confidence);
+}
