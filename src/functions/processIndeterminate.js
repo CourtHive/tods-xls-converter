781c11ba417b0a238c01f56cf1f0af0f30af1f2d
CourtHive <charles@courthive.com>
2022-12-19 09:12:57 -0500

    consider `maxPositionWithValues` for indeterminate

--- src/functions/processIndeterminate.js
+++ src/functions/processIndeterminate.js
@@ -9,13 +9,19 @@ export function processIndeterminate(props) {
   const { sheetDefinition, sheet, sheetNumber, profile, analysis, info } = props;
   if (sheetDefinition && sheetNumber && profile && sheet);
 
+  const { positionColumn } = getPositionColumn(analysis.columnProfiles);
+  const positionProfile = analysis.columnProfiles.find(({ column }) => column === positionColumn);
+  const valuesColumns = analysis.columnProfiles.filter(({ column }) => column !== positionColumn);
+  const maxValueRow = Math.max(...valuesColumns.flatMap(({ rows }) => rows));
+  const maxPositionRow = Math.max(...positionProfile.rows.filter((row) => row <= maxValueRow));
+  const index = positionProfile.rows.indexOf(maxPositionRow);
+  const maxPositionWithValues = positionProfile.values[index];
+
   const hasPosition = Object.values(analysis.attributeMap).includes(POSITION);
   const frequencyValues = Object.values(analysis.columnFrequency);
   const twoOrMoreColumns = frequencyValues.length >= 2;
   const maxFrequencyValue = Math.max(...frequencyValues);
-  const { positionColumnProfile } = getPositionColumn(analysis.columnProfiles);
-  const positionValuesCount = positionColumnProfile?.values?.length || 0;
-  const viableFrequencyColumn = maxFrequencyValue > positionValuesCount / 2;
+  const viableFrequencyColumn = maxFrequencyValue > maxPositionWithValues / 2;
 
   if (hasPosition && twoOrMoreColumns && viableFrequencyColumn) {
     analysis.sheetType = KNOCKOUT;
