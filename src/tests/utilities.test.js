cdd6524fae48031e4ddbac36646f2949de42a97c
CourtHive <charles@courthive.com>
2022-12-30 06:44:25 -0500

    extend test

--- src/tests/utilities.test.js
+++ src/tests/utilities.test.js
@@ -3,15 +3,22 @@ import { hasNumeric } from '../utilities/identification';
 import { expect, it, test } from 'vitest';
 
 const scenarios = [
-  { value: '3-6, 6-3, 7-6(1)', bracketValue: true, seedValue: undefined, nonBracketedValue: '3-6, 6-3, 7-6' },
+  {
+    value: 'participant 3-6, 6-3, 7-6(1)',
+    bracketValue: true,
+    seedValue: undefined,
+    nonBracketedValue: 'participant 3-6, 6-3, 7-6'
+  },
+  { value: '3-6, 6-3, 7-6(1)', bracketValue: false, seedValue: undefined, nonBracketedValue: '3-6, 6-3, 7-6(1)' }, // no alpha leader
   { value: 'participant (1)', bracketValue: true, seedValue: 1, nonBracketedValue: 'participant' },
-  { value: '(1)', bracketValue: true, seedValue: undefined, nonBracketedValue: undefined },
+  { value: 'participant [1]', bracketValue: true, seedValue: 1, nonBracketedValue: 'participant' },
+  { value: '(1)', bracketValue: false, seedValue: undefined, nonBracketedValue: '(1)' }, // no alpha leder
   { value: 1, bracketValue: false, seedValue: undefined, nonBracketedValue: 1 }
 ];
 
 it.each(scenarios)('can recognize and extract seedValues', ({ value, bracketValue, seedValue, nonBracketedValue }) => {
-  expect(getNonBracketedValue(value)).toEqual(nonBracketedValue);
   expect(hasBracketedValue(value)).toEqual(bracketValue);
+  expect(getNonBracketedValue(value)).toEqual(nonBracketedValue);
   expect(getSeeding(value)).toEqual(seedValue);
 });
 
