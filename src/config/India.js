06346f7439d6bc04c824d2bc8519eff53dd5f453
CourtHive <charles@courthive.com>
2023-01-02 07:37:39 -0500

    provider config

--- src/config/India.js
+++ src/config/India.js
@@ -220,7 +220,7 @@ export const config = {
     headerColumns: [
       { attr: POSITION, header: ['#', 'st', 'sr. no', 'sr no', 'sno', 's.n'], valueRegex: '[0-9]+' },
       { attr: ENTRY_STATUS, header: { text: 'st', equals: true }, limit: 1 },
-      { attr: RANKING, header: ['rank', 'co-rank'], limit: 1 },
+      { attr: RANKING, header: ['rank', 'co-rank'], limit: 1, valueRegex: `[0-9]+` },
       { attr: SEED_VALUE, header: ['seed', 'seed no', 'sd', 'sd no', 'sd. no'], limit: 1, valueRegex: `\\d+` },
       {
         attr: LAST_NAME,
