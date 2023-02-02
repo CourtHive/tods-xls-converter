ef952d9334bbd4938ce95c2cda543d1ccc9bbffe
CourtHive <charles@courthive.com>
2023-02-01 20:50:52 -0500

    test

--- live.test.js
+++ live.test.js
@@ -96,7 +96,7 @@ it('can process tests', () => {
   const startIndex = 0;
 
   resetLogging();
-  setLoggingActive(true);
+  setLoggingActive(false);
   setLoggingActive(false, 'singlePositions');
   setLoggingActive(false, 'advanceTargets', {
     roundNumbers: [2],
