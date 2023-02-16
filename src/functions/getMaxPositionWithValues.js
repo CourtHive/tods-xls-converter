e42378adca86982ce63073f15b89de9ea64e58b1
CourtHive <charles@courthive.com>
2023-02-16 10:44:24 -0500

    do not require maxValueRow

--- src/functions/getMaxPositionWithValues.js
+++ src/functions/getMaxPositionWithValues.js
@@ -2,15 +2,17 @@ export function getMaxPositionWithValues({ columnProfiles, positionColumn, analy
   const roundColumns = analysis.columns.round || [];
 
   const positionProfile = columnProfiles.find(({ column }) => column === positionColumn);
+  const positionRows = positionProfile?.rows || [];
+
+  // valuesColumns are participantDetail columns... some providers do not have them.
   const valuesColumns = columnProfiles.filter(
     ({ column, character }) => column !== positionColumn && !roundColumns.includes(column) && character !== 'result'
   );
-  const maxValueRow = Math.max(...valuesColumns.flatMap(({ rows }) => rows));
+  const maxValueRow = Math.max(...valuesColumns.flatMap(({ rows }) => rows), 0);
+  const maxPositionRow = Math.max(...positionRows.filter((row) => !maxValueRow || row <= maxValueRow));
   const valuesCount = valuesColumns.flatMap((c) => c.values).length;
   const rows = columnProfiles.flatMap((c) => c.rows);
   const valuesPerRow = valuesCount / rows.length;
-  const positionRows = positionProfile?.rows || [];
-  const maxPositionRow = Math.max(...positionRows.filter((row) => row <= maxValueRow));
   const index = positionRows.indexOf(maxPositionRow);
   const maxPositionWithValues = positionProfile?.values[index];
   const maxPosition = Math.max(...(positionProfile?.values || []));
