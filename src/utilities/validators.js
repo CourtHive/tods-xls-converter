9ec29c3f4a7145876019988cb2cfa16a9c621980
CourtHive <charles@courthive.com>
2023-03-06 16:25:07 -0500

    add regex validator

--- src/utilities/validators.js
+++ src/utilities/validators.js
@@ -1,3 +1,19 @@
 export function validRanking(value) {
   return /^\d+$/.test(value) || /^MR\d+$/.test(value);
 }
+
+export function isValidRegex(expression) {
+  let parts = expression.split('/'),
+    regex = expression,
+    options = '';
+  if (parts.length > 1) {
+    regex = parts[1];
+    options = parts[2];
+  }
+  try {
+    new RegExp(regex, options);
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
